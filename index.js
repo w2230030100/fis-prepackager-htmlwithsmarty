9a21d3b430469023ef55efc4df99f1062c0d0321
小天天 <781250084@qq.com>
2015-06-23 17:51:54 +0800

    基于fis的插件
    用语类smarty语法的html编写

---
+++ index.js
@@ -0,0 +1,9 @@
+var nfm = require("./lib/nfm");
+
+module.exports = function(ret, conf, settings, opt) {
+    fis.util.map(ret.src, function(subpath, file) {
+        if (file.isHtmlLike) {
+            var nfma = new nfm({file:file});
+        }
+    });
+};
